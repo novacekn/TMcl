50aae014fe8b9ed36cd92cbe071a651467f17164
Nathan Novacek <novacekn@gmail.com>
2024-06-02 13:20:39 -0400

    --

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::{arg, command, value_parser, Arg, ArgAction, Command, ArgMatches, Args, Subcommand};
+
+mod cli;
+
 fn main() {
-    println!("Hello, world!");
+	let matches = cli::build_cli().get_matches();
+
+	match matches.subcommand() {
+		Some(("projects", subcommand)) => {
+			let matches = subcommand.get_matches();
+		}
+		_ => {
+			println!("INVALID");
+		}
+	}
 }
